4945456614b89422c596f322634b4df062ffd5c9
Alla Slyu <alla.slyu@yandex.ru>
2024-06-22 16:07:22 +0300

    5 commit

--- src/main/java/com/git/service/AppService.java
+++ src/main/java/com/git/service/AppService.java
@@ -1,11 +1,19 @@
 package com.git.service;
 
+import com.git.contoller.AppRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppService {
+    //добавить изменение
+    private AppRepository appRepository;
 
     public String hello() {
         return "hello";
     }
+
+//    //изменение не добавлять
+//    public AppRepository getAppRepository() {
+//        return appRepository;
+//    }
 }
